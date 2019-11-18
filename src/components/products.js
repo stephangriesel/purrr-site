f950e5d92f0b3a0e6ec675f2243240d9c004628a
Stephan <sgriesel@gmail.com>
2019-11-18 06:24:15 +0100

    test images

--- src/components/products.js
+++ src/components/products.js
@@ -40,11 +40,16 @@ const Products = () => {
           const skus = allStripeSku.edges.filter(
             sku => sku.node.product.id === product.node.id
           )
+          // const image = allStripeSku.edges.map(product => {
+          //   img => img.node.image === product.node.image
+          // })
           return (
             <Product
               key={product.node.id}
               skus={skus}
-              product={product.node} />
+              product={product.node}
+            // image={image}
+            />
           )
         })
         return
