653fd34e57d39a195ab61dc23239ab8907ded01b
Stephan <sgriesel@gmail.com>
2019-11-29 06:17:32 +0100

    remove key - broken version

--- src/components/product.js
+++ src/components/product.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import '../css/product.css';
 
 const Product = ({ skus, product, image }) => {
-    const stripe = window.Stripe("pk_test_uxVeps2FOxzKRvcPqBMCOftw00gMGfkch3")
+    const stripe = window.Stripe(`${process.env.API_KEY}`)
 
     const [sku, setSku] = useState(skus[0].node.id);
 
