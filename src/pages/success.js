dec1e536db12f263d592e2d2714a2ca8fd9124b5
Stephan <sgriesel@gmail.com>
2019-12-10 06:59:13 +0100

    styles update

--- src/pages/success.js
+++ src/pages/success.js
@@ -5,9 +5,11 @@ import Layout from "../components/layout"
 
 const SuccessPage = () => (
     <Layout>
-        <h1>Success!!</h1>
-        <p>Your order has been placed successfully</p>
-        <Link to="/">Go back to the homepage</Link>
+        <div className="success-page">
+            <h1>Success!!</h1>
+            <p>Your order has been placed successfully</p>
+            <Link to="/">Go back to the homepage</Link>
+        </div>
     </Layout>
 )
 
